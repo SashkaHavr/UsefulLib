b68074bec4e494a2a3d4b14105e19a0e386229c3
Oleksandr <47502748+SashkaHavr@users.noreply.github.com>
2019-04-13 19:05:27 +0200

    TxtLogger

---
+++ UsefulLib/DataAccess/Logger/TXTLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulLib.DataAccess.Logger
+{
+    public class TXTLogger : IFileSaver<string>
+    {
+        public void Save(string obj, string fileName = "Data.bin")
+        {
+            using (var writer = File.AppendText(fileName))
+                writer.Write($"{DateTime.Now.ToString()} - {obj}{Environment.NewLine}");
+
+        }
+    }
+}
